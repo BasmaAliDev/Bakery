2d97f8fcc59d4c80bf962ac8978895782cc75891
BasmaAliDev <basmaalidev@gmail.com>
2024-08-08 05:24:52 +0300

    edit sidebar

--- js/main.js
+++ js/main.js
@@ -1,73 +1,86 @@
 "use strict";
 /**************Loading Screen************/
-$(document).ready(function(){
-    $(".loader").fadeOut(500,function(){
-        $(".loadingScreen").fadeOut(500,function(){
-            $("body").css("overflow","auto");
+$(document).ready(function () {
+    $(".loader").fadeOut(500, function () {
+        $(".loadingScreen").fadeOut(500, function () {
+            $("body").css("overflow", "auto");
             $(".loadingScreen").remove();
         })
-        
+
     })
 })
 /**************navbar background Color when scroll************/
-$(window).scroll(function(){
-    let windowScroll=$(window).scrollTop();
-    if(windowScroll>50){
-        $(".navbar").css("backgroundColor","#fff");
-        $("nav a").css("color"," #686868 ");
-        $(".navbar img").attr("src","images/bakery-color.png");
-        
-    }else{
-        $(".navbar").css("backgroundColor","transparent");
-        $("nav a").css("color","#fff");
-        $(".navbar img").attr("src","images/bakery-light-1.png");
+$(window).scroll(function () {
+    let windowScroll = $(window).scrollTop();
+    if (windowScroll > 50) {
+        $(".navbar").css("backgroundColor", "#fff");
+        $("nav a").css("color", " #686868 ");
+        $(".navbar img").attr("src", "images/bakery-color.png");
+
+    } else {
+        $(".navbar").css("backgroundColor", "transparent");
+        $("nav a").css("color", "#fff");
+        $(".navbar img").attr("src", "images/bakery-light-1.png");
     }
 
 })
 /**************navbar background Color when scroll************/
 
-$("a[href^='#']").click(function(eventInfo){
-    let aHref=eventInfo.target.getAttribute("href");
-    let sectionOffset=$(aHref).offset().top;
-    $("html,body").animate({scrollTop:sectionOffset},500) ;
+$("a[href^='#']").click(function (eventInfo) {
+    let aHref = eventInfo.target.getAttribute("href");
+    let sectionOffset = $(aHref).offset().top;
+    $("html,body").animate({ scrollTop: sectionOffset }, 500);
 })
 /**************arrow Up************/
 
-$(window).scroll(function(){
-    let windowScroll=$(window).scrollTop();
-    if(windowScroll>50){
+$(window).scroll(function () {
+    let windowScroll = $(window).scrollTop();
+    if (windowScroll > 50) {
         $(".arrowUP").fadeIn(500);
-    }else{
+    } else {
         $(".arrowUP").fadeOut(500);
     }
 })
-$(".arrowUP").click(function(){
-    $("html,body").animate({scrollTop:0},500) ;
+$(".arrowUP").click(function () {
+    $("html,body").animate({ scrollTop: 0 }, 500);
 })
 
 /************************side Bar************/
+
+
 let myColors = ['#f0e4d7', '#f5c0c0', '#ff7171', '#9fd8df', '#206a5d', '#81b214', 'black', '#f5f7b2', '#1cc5dc', '#cf0000', '#890596']
-let colorBox=$(".colorBox");
-console.log(colorBox.length);
-for (let i = 0; i <colorBox.length; i++) {
-   colorBox.eq(i).css("backgroundColor",`${myColors[i]}`)
+let colorBox = $(".colorBox");
+
+for (let i = 0; i < colorBox.length; i++) {
+    colorBox.eq(i).css("backgroundColor", `${myColors[i]}`)
 }
-let SideBarInnerWidth=$(".sidBar-inner").innerWidth();
-$("#sideBar").css("left",-SideBarInnerWidth)
-$("#sideBar i").click(function(){
-  
+let SideBarInnerWidth = $(".sidBar-inner").innerWidth();
+$("#sideBar").css("right", -SideBarInnerWidth)
+$("#sideBar i").click(function () {
 
-  if($("#sideBar").css("left")=="0px"){
-    $("#sideBar").animate({left:-SideBarInnerWidth},1000);
-   }
-   else{
-    $("#sideBar").animate({left:"0px"},1000);
-   }
+
+    if ($("#sideBar").css("right") == "0px") {
+        $("#sideBar").animate({ right: -SideBarInnerWidth }, 1000, function () {
+            $("#sideBar i").addClass(".fa fa-cogs");
+            $("#sideBar i").removeClass(".fa-solid fa-xmark");
+        });
+
+    }
+    else {
+        $("#sideBar").animate({ right: "0px" }, 1000, function () {
+            $("#sideBar i").removeClass(".fa fa-cogs");
+            $("#sideBar i").addClass(".fa-solid fa-xmark");
+        });
+
+    }
+
 })
 
-$("#sideBar span").click(function(eventInfo){
-   let colorText=$(eventInfo.target).css("backgroundColor");
-   $("p").css("color",colorText);
+$("#sideBar span").click(function (eventInfo) {
+    let colorText = $(eventInfo.target).css("backgroundColor");
+    $("p").css("color", colorText);
 })
+
+
 
 
