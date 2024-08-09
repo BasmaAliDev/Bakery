d4508e58b9432adc9a9e0a0bb1ecb6d7ea597aa4
BasmaAliDev <basmaalidev@gmail.com>
2024-08-09 05:58:27 +0300

    using <picture> element in navbar

--- js/main.js
+++ js/main.js
@@ -10,13 +10,13 @@ $(document).ready(function () {
     })
 })
 /**************navbar background Color when scroll************/
+
 $(window).scroll(function () {
-    let windowScroll = $(window).scrollTop();
-    if (windowScroll > 50) {
+    let windowScroll = $(window).scrollTop();    
+    if (windowScroll > 50 ) {
         $(".navbar").css("backgroundColor", "#fff");
         $("nav a").css("color", " #686868 ");
         $(".navbar img").attr("src", "images/bakery-color.png");
-
     } else {
         $(".navbar").css("backgroundColor", "transparent");
         $("nav a").css("color", "#fff");
@@ -25,11 +25,7 @@ $(window).scroll(function () {
 
 })
 /****************************************************************/
-if ($(window).width() <= 992) {
-    $(".navbar").css("backgroundColor", "#fff");
-    $("nav a").css("color", "#686868");
-    $(".navbar img").attr("src", "images/bakery-color.png");
-}
+
 document.querySelectorAll('.nav-link').forEach(link => {
     link.addEventListener('click', function () {
         document.querySelector('.navbar-collapse').classList.remove('show');
@@ -65,43 +61,4 @@ $(window).scroll(function () {
 $(".arrowUP").click(function () {
     $("html,body").animate({ scrollTop: 0 }, 500);
 })
-
-/************************side Bar************/
-
-
-let myColors = ['#f0e4d7', '#f5c0c0', '#ff7171', '#9fd8df', '#206a5d', '#81b214', 'black', '#f5f7b2', '#1cc5dc', '#cf0000', '#890596']
-let colorBox = $(".colorBox");
-
-for (let i = 0; i < colorBox.length; i++) {
-    colorBox.eq(i).css("backgroundColor", `${myColors[i]}`)
-}
-let SideBarInnerWidth = $(".sidBar-inner").innerWidth();
-$("#sideBar").css("right", -SideBarInnerWidth)
-$("#sideBar i").click(function () {
-
-
-    if ($("#sideBar").css("right") == "0px") {
-        $("#sideBar").animate({ right: -SideBarInnerWidth }, 1000, function () {
-            $("#sideBar i").addClass(".fa fa-cogs");
-            $("#sideBar i").removeClass(".fa-solid fa-xmark");
-        });
-
-    }
-    else {
-        $("#sideBar").animate({ right: "0px" }, 1000, function () {
-            $("#sideBar i").removeClass(".fa fa-cogs");
-            $("#sideBar i").addClass(".fa-solid fa-xmark");
-        });
-
-    }
-
-})
-
-$("#sideBar span").click(function (eventInfo) {
-    let colorText = $(eventInfo.target).css("backgroundColor");
-    $("p").css("color", colorText);
-})
-
-
-
 
